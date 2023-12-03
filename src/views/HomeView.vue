fd2fe018dcd54c351ef40741884baf0a60b35c7a
ignacio <ignacio.becher@gmail.com>
2023-12-03 13:22:04 -0300

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<template>
+    <navbarComponent></navbarComponent>
+</template>
+
+<script>
+import navbarComponent from '../components/navbarComponent.vue';
+export default {
+    components:{
+        navbarComponent
+    }
+}
+</script>
