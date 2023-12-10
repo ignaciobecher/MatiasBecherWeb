1721bb8dcddfcb0c56bed1eef4315950baaed874
ignacio <ignacio.becher@gmail.com>
2023-12-10 01:56:31 -0300

    from vsc

--- src/components/headerComponent.vue
+++ src/components/headerComponent.vue
@@ -1,40 +1,40 @@
 <template>
   <div id="my-template" class="header-container">
     <video autoplay loop muted playsinline>
-      <source
-        src="../assets/videoComp.mp4"
-        type="video/mp4"
-        preload="auto"
-        lazyload
-      />
+      <source src="../assets/videoComp.mp4" type="video/mp4" preload="auto" lazyload />
     </video>
-    <img
-      src="/images/logoFirma.png"
-      alt="Imagen"
-      style="
-        position: absolute;
-        top: 90%;
-        left: 80%;
-        transform: translate(-50%, -50%);
-        width: 100px;
-      "
-    />
+    <img src="/images/logoFirma.png" alt="Imagen" style="
+                  position: absolute;
+                  top: 90%;
+                  left: 80%;
+                  transform: translate(-50%, -50%);
+                  width: 100px;
+                " />
 
-    <h1
-      style="
-        font-weight: 900;
-        position: absolute;
-        top: 90%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-      "
-    >
-      <i
-        id="btnDown"
-        @click="scrollToBottom"
-        class="bi bi-arrow-down-circle-fill"
-      ></i>
+    <h1 style="
+                  font-weight: 900;
+                  position: absolute;
+                  top: 90%;
+                  left: 50%;
+                  transform: translate(-50%, -50%);
+                ">
+      <i id="btnDown" @click="scrollToBottom" class="bi bi-arrow-down-circle-fill"></i>
     </h1>
+  </div>
+
+  <div class="header-monitor-container">
+    <div class="images">
+      <img loading="eager" src="/images/fotoAbout.jpg" alt="">
+    </div>
+    <div class="images">
+      <img loading="eager" src="/images/headerMain.jpg" alt="">
+    </div>
+    <div class="images">
+      <img loading="eager" src="/images/turismo2.jpg" alt="">
+    </div>
+    <div class="images">
+      <img loading="eager" src="/images/headerMain2.jpg" alt="">
+    </div>
   </div>
 </template>
 
@@ -63,28 +63,65 @@ export default {
   font-family: "Oswald", sans-serif;
 }
 
-.header-container {
-  background-color: #0b1fba;
+/*-------------------------------- RESPONSIVE PARA PANTALLAS GRANDES----------------------------- */
+
+@media (min-width: 761px) {
+  .header-container {
+    display: none;
+  }
+
+  .header-monitor-container {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    grid-template-rows: auto;
+  }
+
+  .images img {
+    width: 100%;
+    height: 100%;
+    filter: grayscale(1);
+    transition: filter 500ms ease-in-out;
+  }
+
+
+
+  .images img:hover {
+    filter: none;
+  }
+
 }
 
-video {
-  width: 100%;
-  filter: brightness(60%);
-}
+/*-------------------------------- RESPONSIVE PARA DISPOSITIVOS PEQUEÑOS------------------------ */
+@media (max-width: 760px) {
+  .header-monitor-container{
+    display: none;
+  }
 
-.nameH1 {
-  color: black;
-  font-weight: 900;
-  position: absolute;
-  top: 70%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  width: 100%;
-  text-align: center;
-}
+  .header-container {
+    background-color: #0b1fba;
 
-#btnDown {
-  color: #0523f6;
-  font-size: 40px;
+  }
+
+  video {
+    width: 100%;
+    filter: brightness(60%);
+  }
+
+  .nameH1 {
+    color: black;
+    font-weight: 900;
+    position: absolute;
+    top: 70%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 100%;
+    text-align: center;
+  }
+
+  #btnDown {
+    color: #0523f6;
+    font-size: 40px;
+  }
+
 }
-</style>
+</style> 
--- src/components/navbarComponent.vue
+++ src/components/navbarComponent.vue
@@ -5,34 +5,27 @@
         <a class="navbar-brand" href="#">
           <img class="logoImg" src="/images/logoBlanco3.png" />
         </a>
-      
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
+
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">
-              <a class="nav-link" @click="scroolToMatias" aria-current="page" href="#">MATIAS BECHER</a>
+              <a class="nav-link underline" @click="scroolToMatias" aria-current="page" href="#">MATIAS BECHER</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" @click="scroolToKarting" href="#">KARTING</a>
-            </li>
-
-            <li class="nav-item">
-              <a class="nav-link" @click="scroolToTurismo">TURISMO PISTA</a>
+              <a class="nav-link underline" @click="scroolToKarting" href="#">KARTING</a>
             </li>
 
-            
             <li class="nav-item">
-              <a class="nav-link" @click="scroolToHistory">MI HISTORIA</a>
+              <a class="nav-link underline" @click="scroolToTurismo" href="#">TURISMO PISTA</a>
+            </li>
+
+
+            <li class="nav-item">
+              <a class="nav-link underline" @click="scroolToHistory" href="#">MI HISTORIA</a>
             </li>
           </ul>
         </div>
@@ -46,34 +39,35 @@ export default {
   methods: {
     scroolToMatias() {
       const height = document.getElementById("my-template").clientHeight
-      window.scrollTo(0, height+100);
+      window.scrollTo(0, height + 100);
     },
-    scroolToKarting(){
+    scroolToKarting() {
       const height = document.getElementById("my-template").clientHeight
-      window.scrollTo(0,height+950)
+      window.scrollTo(0, height + 950)
     },
-    scroolToTurismo(){
+    scroolToTurismo() {
       const height = document.getElementById("my-template").clientHeight
-      window.scrollTo(0,height+1550)
+      window.scrollTo(0, height + 1550)
     },
-    scroolToHistory(){
+    scroolToHistory() {
       const height = document.getElementById("my-template").clientHeight
-      window.scrollTo(0,height+2200)
+      window.scrollTo(0, height + 2200)
     },
-    
+
   },
 };
 </script>
 
 <style scoped>
-
 @import url('https://fonts.googleapis.com/css2?family=Oswald:wght@500&display=swap');
 
 * {
   font-family: 'Oswald', sans-serif;
 }
 
-.logoImg{
+
+@media (min-width: 761px) {
+  .logoImg{
   filter: brightness(95%);
 }
 
@@ -102,5 +96,61 @@ nav .nav-link{
   background-color: #0b1fba ;
   border-radius: 5px;
   
+}
+/* .underline{
+  position: relative;
+}
+
+.underline::before{
+  content: '';
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  width: 0;
+  height: 2px;
+  background-color: white;
+  transition: width 0.6s cubic-bezier(0.25, 1, 0.5, 1);
+} */
+
+/* @media (hover: hover) and (pointer: fine) {
+  .underline:hover::before{
+    left: 0;
+    right: auto;
+    width: 100%;
+  }
+} */
+} 
+
+@media (max-width: 760px) {
+  .logoImg {
+    filter: brightness(95%);
+  }
+
+  nav {
+    background-color: #0b1fba !important;
+    height: 15vh;
+    z-index: 1;
+  }
+
+  nav .nav-link {
+    font-weight: 900;
+    font-size: 23px;
+    color: white;
+  }
+
+  .navbar img {
+    width: 80px;
+  }
+
+  .navbar-toggler {
+    background-color: #0b1fba;
+    border: none;
+  }
+
+  .navbar-toggler-icon {
+    background-color: #0b1fba;
+    border-radius: 5px;
+
+  }
 }
 </style>
